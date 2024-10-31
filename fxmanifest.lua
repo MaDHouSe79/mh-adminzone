e3c742d37e63fa79158075fdcf5828481338e72b
MaDHouSe <madhouse1979@gmail.com>
2024-10-31 22:29:30 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,10 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'Nick Perry'
+author ''
 description 'Admin Zone'
-version '1.0.4'
+version '1.0.0'
 
-shared_scripts {
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua', -- change en to your language
-    'config.lua',
-}
-
-client_script 'client/main.lua'
-server_script 'server/main.lua'
+shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', 'config.lua',}
+client_scripts {'client/main.lua',}
+server_scripts {'server/main.lua', 'server/update.lua',}
